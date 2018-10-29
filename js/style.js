7245c94863f8b8f28069885495db1a3accba4279
vanessa <nessastagg@gmail.com>
2018-10-29 19:28:59 -0300

    adicionado css framework bulma

---
+++ js/style.js
@@ -0,0 +1,10 @@
+// Initialize all input of date type.
+var calendars = bulmaCalendar.attach('[type="date"]', {dateFormat: 'DD/MM/YYYY'});
+
+// Loop on each calendar initialized
+for(var i = 0; i < calendars.length; i++) {
+	// Add listener to date:selected event
+	calendars[i].on('date:selected', date => {
+		console.log(date);
+	});
+}
